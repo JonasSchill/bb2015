9addeffda3b844d79f235ed737fdbf5d4e0796ed
tchatow <tchatow@gmail.com>
2015-02-15 20:15:05 -0800

    Added claw actions in drive.c
    I have created all of the actions for the claw and started on the
    driving actions for drive.c

--- Regionals 2015/LINK/Seeding/drive.c
+++ Regionals 2015/LINK/Seeding/drive.c
@@ -1,3 +1,41 @@
 #include "drive.h"
 
 //write driving related functions here
+
+//			Claw actions
+//		up
+	void raiseClaw () {
+		set_servo_position(SERVO_UP_DOWN,CLAW_UP);
+	}
+//		down
+	void lowerClaw () {
+		set_servo_position(SERVO_UP_DOWN,CLAW_DOWN);
+	}
+//		open
+	void openClaw () {
+		set_servo_position(SERVO_OPEN_CLOSE,CLAW_OPEN);
+	}
+	
+//		close
+	void closeClaw () {
+		set_servo_position(SERVO_OPEN_CLOSE,CLAW_CLOSE);
+	}
+	
+//		dump
+	void dumpClaw () {
+		raiseClaw();
+		openClaw();
+		closeClaw();
+		openClaw();
+		closeClaw();
+		openClaw();
+		closeClaw();
+	}
+
+//			Driving
+//	void driveForward(Speed,Distance) {
+//		motor(MOTOR_LEFT,driveForward(float Speed);
+//		msleep();
+//		motor(MOTOR_RIGHT,driveForward(float Speed);
+//		msleep();
+//	}
