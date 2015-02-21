22bc54322bdf1e4ac4846f5858d9df2316e91d0d
andywang1209 <andywang02@gmail.com>
2015-02-20 16:27:04 -0800

    Started Create's drive.c

--- Regionals 2015/CREATE/Seeding/createDrive.c
+++ Regionals 2015/CREATE/Seeding/createDrive.c
@@ -1,3 +1,12 @@
 #include "drive.h"
 
 //write driving related functions here
+
+ void raiseArm() {
+	clear_motor_position_counter()
+	while(get_motor_position_counter(MOTOR_UP_DOWN)<---) {
+		//--- means unknown number
+		//I need to add in the part where you can adjust the height of the arm
+		motor(MOTOR_UP_DOWN,MOTOR_UP_DOWN_SPEED);
+	}
+ }
--- Regionals 2015/CREATE/Seeding/createDrive.h
+++ Regionals 2015/CREATE/Seeding/createDrive.h
@@ -1,1 +1,4 @@
 //define things for createDrive.c here
+
+#define MOTOR_UP_DOWN 1     //This probably isn't the right port or speed
+#define MOTOR_UP_DOWN_SPEED 200
