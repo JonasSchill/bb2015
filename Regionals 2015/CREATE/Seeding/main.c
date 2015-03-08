b4a8eab7a2a93e42202a656d2bfe48fdad3b3bb9
tchatow <tchatow@gmail.com>
2015-03-08 14:17:22 -0700

    Moved main to dump sequence

--- Regionals 2015/CREATE/Seeding/main.c
+++ Regionals 2015/CREATE/Seeding/main.c
@@ -3,6 +3,16 @@
 //main file gets run by robot calls methods in all other files
 
 int main() {
+	
+}
+
+	
+void create_Main() {
+	//msleep(15000); //wait for LINK blocks dump
+	createTurnLeft(90);
+}
+
+void dumpSequence() {
 	enableDevices();
 	raiseLowerArmNew(1300);
 	msleep(2000);
@@ -12,10 +22,4 @@ int main() {
 	createBasketDump();
 	createTurnLeft(15);
 	createDriveBack(100,50);
-}
-
-	
-void create_Main() {
-	//msleep(15000); //wait for LINK blocks dump
-	createTurnLeft(90);
 }
