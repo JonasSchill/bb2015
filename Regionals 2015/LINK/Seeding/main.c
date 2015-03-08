02eb892fb96e20a01aff27973d5d63e75c9edba4
awokerw <awokerw@gmail.com>
2015-03-07 18:18:00 -0800

    completed main (works perfectly)

--- Regionals 2015/LINK/Seeding/main.c
+++ Regionals 2015/LINK/Seeding/main.c
@@ -1,13 +1,36 @@
 #include "template.h"
 
 int main() {
-/*	enableAllServos();
+
+	enableAllServos();
 	raiseClaw();
-	msleep(500);
 	closeClaw();
+	msleep(500);
+	
 	driveForward(103.34,1);
 	turnRight(90);
 	driveForward(40.5,1);
-	turnRight(90);*/
-	driveUntilLine(1);
+	turnRight(90);
+	
+	lowerClaw();
+	openClaw();
+	driveForward(80, 1);
+	closeClaw();
+	raiseClaw();
+	driveBackward(80, 1);
+	
+	turnRight(-90);
+	turnLeft(90);
+	
+	lowerClaw();
+	openClaw();
+	driveForward(80, 1);
+	closeClaw();
+	raiseClaw();
+	driveBackward(80, 1);
+	
+	turnLeft(90);
+	driveBackward(40, 1);
+	turnRight(90);
+	driveForward(103, 1);
 }
