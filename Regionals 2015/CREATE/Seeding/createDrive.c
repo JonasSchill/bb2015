5d02897bed0e79d896105b02710b69838ca6d5b4
awokerw <awokerw@gmail.com>
2015-03-08 11:28:40 -0700

    fixed typo

--- Regionals 2015/CREATE/Seeding/createDrive.c
+++ Regionals 2015/CREATE/Seeding/createDrive.c
@@ -20,25 +20,27 @@ void raiseLowerArmNew(int destination) {
 void createDrive (float speed, float distance) {
 	set_create_distance(0);
 	create_drive_direct(speed,speed);
-	while (get_create_drive_distance*10 <= distance){}
+	while (get_create_drive_distance*10 <= distance){
+	}
 	create_stop();
+	set_create_distance(0);
 }
 
 void createTurnLeft(int degrees) {
 	set_create_normalized_angle(0); //Reset the angle
 	create_spin_CCW(250); //Spin at half power
-	while(get_create_normalized_angle() <= degrees - 20) {} //go most of the distance
+	while(get_create_normalized_angle() < degrees - 20) {} //go most of the distance
 	create_spin_CCW(50); //slow down as to not overshoot
-	while(get_create_normalized_angle() <= degrees) {} //and finish the turn
+	while(get_create_normalized_angle() < degrees) {} //and finish the turn
 	create_stop();
 }
 
 void createTurnRight(int degrees) {
 	set_create_normalized_angle(degrees); //Reset the angle
 	create_spin_CW(250); //Spin at half power
-	while(get_create_normalized_angle() >= 20) {} //go most of the distance
+	while(get_create_normalized_angle() > 20) {} //go most of the distance
 	create_spin_CW(50); //slow down as to not overshoot
-	while(get_create_normalized_angle() >= 0) {} //and finish the turn
+	while(get_create_normalized_angle() > 0) {} //and finish the turn
 	create_stop();
 }
 
