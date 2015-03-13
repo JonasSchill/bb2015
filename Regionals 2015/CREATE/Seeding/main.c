dcad2cceb08a4cc6eaf029391e27359f900a14fc
andywang1209 <andywang02@gmail.com>
2015-03-13 09:56:31 -0700

    Started ping pong sequence THIS IS INCOMPLETE

--- Regionals 2015/CREATE/Seeding/main.c
+++ Regionals 2015/CREATE/Seeding/main.c
@@ -2,10 +2,6 @@
 
 //main file gets run by robot calls methods in all other files
 
-int main() {
-	
-}
-
 void dumpSequence() {
 	enableDevices();
 	raiseLowerArmNew(1300);
@@ -18,17 +14,31 @@ void dumpSequence() {
 	createDriveBack(100,50);
 }
 
-void pingpongsequence(){
-	enableDevices();
-	raiseLowerArmNew (530);
-	createDriveBack (100, 7);
-	createTurnLeft (90);
-	createDrive (100,30);
-	createTurnRight (90);
+void pingpongsequence() {
+	enableDevices();	//enables devices
+	raiseLowerArmNew (300);		//drives with raised arm
+	msleep(1000);
+	createDrive (100, 21);
+	raiseLowerArmNew (780);		//picks up front ping pong
+	msleep(1500);
+	createSquareUp(100,3.6);	//gets in pos. for next ping pong
+	raiseLowerArmNew (1240);	//picks up next ping pong
+	msleep(1500);
+	createSquareUp (100,1.5);	
+	raiseLowerArmNew (1690);
+	msleep(1000);
+	createDriveBack (100,9);	//gets in pos. for next ping pong
+	createTurnLeft (15);
+	/*
+	createDrive(100,8);			//picks up next one
 	raiseLowerArmNew(800);
-	createDriveBack (100, 7);
-	createTurnLeft (90);
-	createDrive (100,30);
-	createTurnRight (90);
-	raiseLowerArmNew(1200);
+	createDriveBack (100,7);	//gets in pos. for next one
+	createTurnLeft (15);
+	createDrive (100,21);
+	raiseLowerArmNew(1000);		//picks up next one
+	*/
+}
+
+int main() {
+	dumpSequence();
 }
