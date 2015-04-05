b79108d9240a93a1c92bf7f2b3a9878d75486885
andywang1209 <andywang02@gmail.com>
2015-04-04 21:23:02 -0700

    added some main.c

--- Regionals 2015/CREATE/Seeding/main.c
+++ Regionals 2015/CREATE/Seeding/main.c
@@ -1,6 +1,42 @@
-#include "template.h"
+armMove#include "template.h"
 
 //main file gets run by robot calls methods in all other files
+
+void pingPongSequence() {
+	//raiseLowerArm(ARM_HIGH - 200, 0.5);		//drives with raised arm
+	//createDrive(100, 26);
+	armMove(ARM_HIGH - 400, 0.5, 100, 25);
+	raiseLowerArm(ARM_HIGH, 1);
+	msleep(500);
+	//createDriveBack(100, 8);
+	//msleep(500);
+	//raiseLowerArm(ARM_MEDIUM - 200, 1);
+	//msleep(500);
+	armMove(ARM_MEDIUM - 200, 1, -100, 8);
+	createTurnLeft(21);
+	createDrive(100, 10);
+	raiseLowerArm(ARM_MEDIUM, 1);
+	msleep(500);
+	//createDriveBack(100, 12);
+	//msleep(500);
+	//raiseLowerArm(ARM_LOW - 100, 1);
+	armMove(ARM_LOW - 100, 1, -100, 12);
+	msleep(500);
+	createTurnLeft(16);
+	createDrive(100, 22);
+	raiseLowerArm(ARM_LOW, 1);
+	createDriveBack(100, 15);
+}
+
+void preparePingPong {
+	createTurnLeft(49);
+	createDrive(100, 135);
+	createTurnLeft(90);
+	createSquareUp(100,3);
+	createDriveBack(80,5);
+	createTurnRight(180);	
+	createDriveBack(80,5);
+}
 
 void dumpSequence() {
 	enableDevices();
@@ -15,41 +51,14 @@ void dumpSequence() {
 	raiseLowerArm(320, 1);
 }
 
-/*void createMain() { //This is being programmed for the actual competion now
-	//raiseLowerArm(ARM_HIGH - 200, 0.5);		//drives with raised arm
-	//createDrive(100, 26);
-	driveAndServo(ARM_HIGH - 400, 0.5, 100, 25);
-	raiseLowerArm(ARM_HIGH, 1);
-	msleep(500);
-	//createDriveBack(100, 8);
-	//msleep(500);
-	//raiseLowerArm(ARM_MEDIUM - 200, 1);
-	//msleep(500);
-	driveAndServo(ARM_MEDIUM - 200, 1, -100, 8);
-	createTurnLeft(21);
-	createDrive(100, 10);
-	raiseLowerArm(ARM_MEDIUM, 1);
-	msleep(500);
-	//createDriveBack(100, 12);
-	//msleep(500);
-	//raiseLowerArm(ARM_LOW - 100, 1);
-	driveAndServo(ARM_LOW - 100, 1, -100, 12);
-	msleep(500);
-	createTurnLeft(16);
-	createDrive(100, 22);
-	raiseLowerArm(ARM_LOW, 1);
-	createDriveBack(100, 15);
-	createTurnLeft(49);
-	createDrive(175, 25);
-	//createTurnRight(90);
-}*/
+void createMain() { //This is being programmed for the actual competion now
+	pingPongSequence();
+	preparePingPong();
+	pingPongSequence();
+}
 
 int main() {
 	enableDevices();
-	//createMain();
-	raiseLowerArm(900, 3000);
-	/*int offset = 75;
-	set_servo_position(SERVO_UP_DOWN_RIGHT, ARM_HIGH + offset);
-	set_servo_position(SERVO_UP_DOWN_LEFT, 2047 - ARM_HIGH - offset);*/
+	createMain();
 	return 0;
 }
