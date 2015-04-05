1a3ffb13f93a8b5d6e2fe411c59630ff2f2db104
SassyQuatch <maxsimpson0@gmail.com>
2015-04-04 21:31:32 -0700

    Tweaked code for link--improved consistency

--- Regionals 2015/LINK/Seeding/drive.h
+++ Regionals 2015/LINK/Seeding/drive.h
@@ -10,7 +10,7 @@
 #define CLAW_OPEN CLAW_CLOSED+950
 #define CLAW_DOWN CLAW_UP+605
 #define CLAW_OPEN_READY CLAW_CLOSED+600
-#define CLAW_UP_READY CLAW_CLOSED+250
+#define CLAW_UP_READY 160
 #define LS_LEFT 1
 #define LS_RIGHT 0
 #define TAPE_VALUE 880
@@ -19,12 +19,12 @@
 
 //tuning constants
 #define DrFLC 100/LEFT_FULL_POWER
-#define DrFRC 93/RIGHT_FULL_POWER
+#define DrFRC 92/RIGHT_FULL_POWER
 #define DrBRC 96/RIGHT_FULL_POWER
 #define DrBLC 100/LEFT_FULL_POWER
 #define Rt .98 
 #define Lt .94
-#define CLAW_CLOSED 130
+#define CLAW_CLOSED 650
 #define CLAW_UP 0
 
 //tune this value to make driving as accurate as possible
